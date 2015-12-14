954faa916cdff0c3c5310437674ceca18100eb0b
Aakash Dhar <aakashdhar@ymail.com>
2015-12-14 22:19:44 +0530

    optimized css and js

--- js/shantivan.js
+++ js/shantivan.js
@@ -1,26 +1,2 @@
-$(function() {
-	    $('a.page-scroll').bind('click', function(event) {
-	        var $anchor = $(this);
-	        $('html, body').stop().animate({
-	            scrollTop: $($anchor.attr('href')).offset().top
-	        }, 2000, 'easeInOutExpo');
-	        event.preventDefault();
-	    });
-	});
-
-	// Highlight the top nav as scrolling occurs
-	$('body').scrollspy({
-	    target: '.navbar-fixed-top'
-	})
-	// maps
-	new GMaps({
-	  div: '.map',
-	  lat: -12.043333,
-	  lng: -77.028333
-	});
-
-
-	// Closes the Responsive Menu on Menu Item Click
-	$('.navbar-collapse ul li a').click(function() {
-	    $('.navbar-toggle:visible').click();
-});
+$(function(){$('a.page-scroll').bind('click',function(event){var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},2000,'easeInOutExpo');event.preventDefault();});});$('body').scrollspy({target:'.navbar-fixed-top'})
+new GMaps({div:'.map',lat:-12.043333,lng:-77.028333});$('.navbar-collapse ul li a').click(function(){$('.navbar-toggle:visible').click();});
